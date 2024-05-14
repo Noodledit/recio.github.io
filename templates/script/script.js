765407d1b35ea9450cf6ce5566d44d17668a2da3
Noodledit <hblanco.nico@gmail.com>
2024-05-14 18:03:05 -0300

    arregle algunos problemas, pero no todos, hay que seguir agregando contenido, espero estes conmigo en esto, te extraño

--- templates/script/script.js
+++ templates/script/script.js
@@ -1,4 +1,30 @@
 
+let myHeader = `
+    <div id="Logo" class="grid-item">
+        <h1><span>#</span>Recio</h1>
+    </div>
+    <!-- <img src="/static/img/traced-parthenon-facts-5.png" alt=""> -->
+    <nav id="menu" class="grid-item">
+        <div class=""><button data-target="#inicio" class="flex-item button navegacion actual">Inicio</button></div>
+        <div class=""><button data-target="#html" class="flex-item button navegacion">HTML</button></div>
+        <div class=""><button data-target="#css" class="flex-item button navegacion">CSS</button></div>
+        <div class=""><button data-target="#js" class="flex-item button navegacion">JavaScript</button></div>
+    </nav>
+    <div class="grid-item button-container">
+        <button class="butt-flex">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    </div>
+`
+document.querySelector("header").innerHTML = myHeader
+
+let myFooter = `
+
+`
+document.querySelector("footer").innerHTML = myFooter
+
 const targets = document.querySelectorAll('[data-target]')
 const content = document.querySelectorAll('[data-content]')
 targets.forEach(target => {
@@ -6,14 +32,12 @@ targets.forEach(target => {
     target.addEventListener('click', () => {
         targets.forEach(g => {
             g.classList.remove('actual')
-            g.classList.remove('borderGradient')
         })
         content.forEach(c => {
             c.classList.remove('activo') 
         })
 
         target.classList.add('actual')
-        target.classList.add('borderGradient')
 
         const t = document.querySelector(target.dataset.target)
         t.classList.add('activo')
