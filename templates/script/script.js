0c2850193a3d6f855b41ba52b2dfdaa94d928ed3
Noodledit <hblanco.nico@gmail.com>
2024-05-17 00:04:56 -0300

    me tarde bastante pero aca te dejo el diseño de los botones y el logo sin terminar, espero tu commit aun >.<

--- templates/script/script.js
+++ templates/script/script.js
@@ -1,22 +1,23 @@
 
 let myHeader = `
-    <div id="Logo" class="grid-item">
-        <h1><span>#</span>Recio</h1>
-    </div>
-    <!-- <img src="/static/img/traced-parthenon-facts-5.png" alt=""> -->
-    <nav id="menu" class="grid-item">
-        <div class=""><button data-target="#inicio" class="flex-item button navegacion actual">Inicio</button></div>
-        <div class=""><button data-target="#html" class="flex-item button navegacion">HTML</button></div>
-        <div class=""><button data-target="#css" class="flex-item button navegacion">CSS</button></div>
-        <div class=""><button data-target="#js" class="flex-item button navegacion">JavaScript</button></div>
-    </nav>
-    <div class="grid-item button-container">
-        <button class="butt-flex">
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
-    </div>
+<div id="logo">
+<img class="logo" src="/static/img/Background-Parthenon.png" alt="Background-Parthenon">
+<div class="logoGradiente"></div>
+<img src="../static/img/#Recio.jpeg" alt="">
+</div>
+<nav id="menu">
+    <div class=""><button data-target="#inicio" class="navegacion actual">Inicio</button></div>
+    <div class=""><button data-target="#html" class="navegacion">HTML</button></div>
+    <div class=""><button data-target="#css" class="navegacion">CSS</button></div>
+    <div class=""><button data-target="#js" class="navegacion">JavaScript</button></div>
+</nav>
+<div class="button-container">
+    <button class="butt-flex">
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
+</div>
 `
 document.querySelector("header").innerHTML = myHeader
 
