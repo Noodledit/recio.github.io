e78c96828fae20a6e6f2df3e7fc0bc2066d7cfa1
Noodledit <hblanco.nico@gmail.com>
2024-05-21 03:38:39 -0300

    contenido de html, arreglo a medias de logo, agregado felino, princess tuallete is my favorite person in the world

--- templates/script/script.js
+++ templates/script/script.js
@@ -1,30 +1,33 @@
 
-let myHeader = `
-<div id="logo">
-<img class="logo" src="/static/img/Background-Parthenon.png" alt="Background-Parthenon">
-<div class="logoGradiente"></div>
-<img src="../static/img/#Recio.jpeg" alt="">
-</div>
-<nav id="menu">
-    <div class=""><button data-target="#inicio" class="navegacion actual">Inicio</button></div>
-    <div class=""><button data-target="#html" class="navegacion">HTML</button></div>
-    <div class=""><button data-target="#css" class="navegacion">CSS</button></div>
-    <div class=""><button data-target="#js" class="navegacion">JavaScript</button></div>
-</nav>
-<div class="button-container">
-    <button class="butt-flex">
-        <span></span>
-        <span></span>
-        <span></span>
-    </button>
-</div>
-`
-document.querySelector("header").innerHTML = myHeader
+ let myHeader = `
+<div id="logoContenedor">
+            <div id="logoGradiente">
+                <span id="logo">
+                <a id="letras"></a>
+                </span>
+            </div>
+        </div>
+        <a id="gato"></a>
+        <nav id="menu">
+            <div class=""><button data-target="#inicio" class="navegacion actual">Inicio</button></div>
+            <div class=""><button data-target="#html" class="navegacion">HTML</button></div>
+            <div class=""><button data-target="#css" class="navegacion">CSS</button></div>
+            <div class=""><button data-target="#js" class="navegacion">JavaScript</button></div>
+        </nav>
+        <div class="button-container">
+            <button class="butt-flex">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+ `
+ document.querySelector("header").innerHTML = myHeader
 
-let myFooter = `
-
-`
-document.querySelector("footer").innerHTML = myFooter
+// let myFooter = `
+// <p>Soy un footer y agrego este texto para ver como queda al final por el asunto del desglose</p>
+// `
+// document.querySelector("footer").innerHTML = myFooter
 
 const targets = document.querySelectorAll('[data-target]')
 const content = document.querySelectorAll('[data-content]')
@@ -35,14 +38,34 @@ targets.forEach(target => {
             g.classList.remove('actual')
         })
         content.forEach(c => {
-            c.classList.remove('activo') 
+            c.classList.remove('activo')
         })
 
         target.classList.add('actual')
 
         const t = document.querySelector(target.dataset.target)
         t.classList.add('activo')
-        
+
     })
 }
 )
+
+var contenedor = document.getElementById('logo');
+var imagen = document.getElementById('letras');
+
+function deslizarImagen() {
+    var x = Math.random() * (contenedor.offsetWidth - imagen.offsetWidth);
+    var y = Math.random() * (contenedor.offsetHeight - imagen.offsetHeight);
+
+    imagen.style.left = x + 'px';
+    imagen.style.top = y + 'px';
+}
+
+setInterval(deslizarImagen, 2000);  // Desliza la imagen cada 2 segundos
+
+const botonFlotante = document.querySelector('.boton-flotante');
+const menu = document.querySelector('.navbar');
+
+botonFlotante.addEventListener('click', () => {
+  menu.classList.toggle('mostrar-menu'); // Agrega o quita la clase 'mostrar-menu'
+});
